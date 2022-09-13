d2a49949e939b4daaedd897d5fcd2f22ae387218
luminitatest <luminita.boca@gmail.com>
2022-09-13 11:06:58 +0300

    commit Truck

---
+++ src/Truck.java
@@ -0,0 +1,25 @@
+/*
+Create a sub class of Car class and name it as Truck. The Truck class has the following fields and methods.
+Creati o sub clasa a clasei Car, numita Truck (Camion). Clasa Truck are urmatoarele campuri si metode:
+	◦int weight;
+	◦double getSalePrice(); //If weight>2000, 10%discount Otherwise no discount.
+
+ */
+public class Truck extends Car{
+    private int weight;
+
+  public Truck(int speed, double regularPrice, String color, int weight){
+      super(speed,regularPrice,color);
+      this.weight = weight;
+  }
+    public double getSalePrice() {
+
+        if (weight > 2000){
+            return super.getSalePrice() - (0.1 * super.getSalePrice());
+        }
+        else {
+            return super.getSalePrice();
+        }
+    }
+  }
+
